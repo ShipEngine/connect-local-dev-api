eb6fc1e6eecdf9ddcd6343e750f7f2e82ad10ba8
Pierce Harmon <pierceh.dev@gmail.com>
2020-08-07 11:59:24 -0500

    W.I.P.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
-// import errorHandler from 'errorhandler';
+import express from "express";
 import cors from "cors";
+import { loadApp } from "@shipengine/integration-platform-loader";
+import { SdkApp } from "./types";
+import buildAPI from "./build-api";
 import log from "./utils/logger";
-import app from "./app";
 
-export function server(port: number, pathToApp: string): void {
+export async function server(port: number, pathToApp: string): Promise<void> {
+  const app = express();
+
   app.use(cors());
+
+  try {
+    const sdkApp = (await loadApp(pathToApp)) as SdkApp;
+    buildAPI(sdkApp, app);
+  } catch (error) {
+    // TODO - consider how this should work
+    // We want an app to have the ability to boot up even if it is not valid
+    // But I think we only want to boot up if we are in a valid app directory
+    console.log("Error building API from SDK");
+    console.log(error.stack);
+  }
 
   app.listen(port, () => log(`server running at http://localhost:${port}`));
 }
---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+import {
+  OrderApp,
+  CarrierApp,
+} from "@shipengine/integration-platform-sdk/lib/internal";
+
+import { AppType } from "@shipengine/integration-platform-sdk";
+
+export type SdkApp = CarrierApp | OrderApp;
+
+export type SdkAppTypes = AppType.Carrier | AppType.Order;
