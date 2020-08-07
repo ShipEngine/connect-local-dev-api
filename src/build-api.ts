3e9ca9e3ae73bf8ef19b7958574db002709bf456
Pierce Harmon <pierceh.dev@gmail.com>
2020-08-07 14:50:47 -0500

    W.I.P.

--- src/build-api.ts
+++ src/build-api.ts
@@ -14,11 +14,11 @@ export default function buildAPI(sdkApp: CarrierApp, server: Express) {
 
   sdkApp.connect && server.put("/connect", connect);
   sdkApp.createShipment && server.put("/create-shipment", createShipment);
+  sdkApp.cancelShipments && server.put("/cancel-shipments", cancelShipments);
 
-  // sdkApp.cancelShipments && server.put("/cancelShipments", cancelShipments);
   // sdkApp.rateShipment && server.put("/rateShipment", rateShipment);
   // sdkApp.trackShipment && server.put("/trackShipment", trackShipment);
-  // // sdkApp.createManifest && server.put("/createManifest", createManifest);
+  // sdkApp.createManifest && server.put("/createManifest", createManifest);
   // sdkApp.schedulePickup && server.put("/schedulePickup", schedulePickup);
   // sdkApp.cancelPickups && server.put("/cancelPickups", cancelPickups);
 
@@ -44,65 +44,66 @@ export default function buildAPI(sdkApp: CarrierApp, server: Express) {
       shipment = await sdkApp.createShipment!(transaction, shipment);
       return res.status(200).send({
         transaction,
+        shipment,
       });
     } catch (error) {
       return res.status(400).send(error);
     }
   }
 
-  // async function cancelShipments(req: Request, res: Response) {
-  //   try {
-  //     const { transaction, shipments } = req.body;
-  //     const outcomes = await server.cancelShipments!(
-  //       transaction,
-  //       shipments,
-  //     );
-  //     send(req, res, 200, { transaction, outcomes });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
+  async function cancelShipments(req: Request, res: Response) {
+    try {
+      const { transaction, shipments } = req.body;
+      const outcomes = await sdkApp.cancelShipments!(transaction, shipments);
+      return res.status(200).send({
+        transaction,
+        outcomes,
+      });
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
 
   // async function rateShipment(req: Request, res: Response) {
   //   try {
   //     const { transaction, shipment } = req.body;
-  //     const rates = await server.rateShipment!(transaction, shipment);
+  //     const rates = await sdkApp.rateShipment!(transaction, shipment);
   //     send(req, res, 200, { transaction, rates });
   //   } catch (error) {
-  //     next(error);
+  //     return res.status(400).send(error);
   //   }
   // }
 
   // async function trackShipment(req: Request, res: Response) {
   //   try {
   //     const { transaction, shipment } = req.body;
-  //     const trackingInfo = await server.trackShipment!(
+  //     const trackingInfo = await sdkApp.trackShipment!(
   //       transaction,
   //       shipment,
   //     );
   //     send(req, res, 200, { transaction, trackingInfo });
   //   } catch (error) {
-  //     next(error);
+  //     return res.status(400).send(error);
   //   }
   // }
 
   // async function schedulePickup(req: Request, res: Response) {
   //   try {
   //     let { transaction, pickup } = req.body;
-  //     pickup = await server.schedulePickup!(transaction, pickup);
+  //     pickup = await sdkApp.schedulePickup!(transaction, pickup);
   //     send(req, res, 200, { transaction, pickup });
   //   } catch (error) {
-  //     next(error);
+  //     return res.status(400).send(error);
   //   }
   // }
 
   // async function cancelPickups(req: Request, res: Response) {
   //   try {
   //     let { transaction, pickups } = req.body;
-  //     pickups = await server.cancelPickups!(transaction, pickups);
+  //     pickups = await sdkApp.cancelPickups!(transaction, pickups);
   //     send(req, res, 200, { transaction, pickups });
   //   } catch (error) {
-  //     next(error);
+  //     return res.status(400).send(error);
   //   }
   // }
 }
--- test/specs/build-api.spec.ts
+++ test/specs/build-api.spec.ts
@@ -175,4 +175,70 @@ describe("buildAPI", () => {
         expect(res.body).to.eqls({});
       });
   });
+
+  xit("sets the PUT '/cancel-shipments' endpoint and returns 200 when given a valid input", async () => {
+    const server = express();
+    const app = (await loadApp("test/fixtures/carrier-app")) as CarrierApp;
+
+    buildAPI(app, server);
+
+    chai
+      .request(server)
+      .put("/cancel-shipments")
+      .send({
+        transaction: {
+          id: "6ad41b24-62a8-4e17-9751-a28d9688e277",
+          session: {},
+        },
+        outcomes: {},
+      })
+      .end((_err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.eql({
+          transaction: {
+            id: "6ad41b24-62a8-4e17-9751-a28d9688e277",
+            session: {},
+          },
+        });
+      });
+  });
+
+  it("sets the PUT '/cancel-shipments' endpoint and returns 400 when given an invalid input", async () => {
+    const server = express();
+    const app = (await loadApp("test/fixtures/carrier-app")) as CarrierApp;
+
+    buildAPI(app, server);
+
+    chai
+      .request(server)
+      .put("/cancel-shipments")
+      .send({
+        transaction: {
+          id: "6ad41b24-62a8-4e17-9751-a28d9688e277",
+          session: {},
+        },
+        outcomes: {},
+      })
+      .end((_err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.body).to.haveOwnProperty("code", "ERR_INVALID_INPUT");
+      });
+  });
+
+  it("does not set the PUT '/cancel-shipments' endpoint when connect is not defined", async () => {
+    const server = express();
+    const app = (await loadApp(
+      "test/fixtures/carrier-app-without-methods",
+    )) as CarrierApp;
+
+    buildAPI(app, server);
+
+    chai
+      .request(server)
+      .put("/cancel-shipments")
+      .end((_err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.body).to.eqls({});
+      });
+  });
 });
