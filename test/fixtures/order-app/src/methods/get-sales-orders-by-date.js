6719b2145b3af5fdc15fafc594d8ed810f4de7c3
Pierce Harmon <pierceh.dev@gmail.com>
2020-09-11 13:33:16 -0500

    W.I.P.

--- test/fixtures/order-app/src/methods/get-sales-orders-by-date.js
+++ test/fixtures/order-app/src/methods/get-sales-orders-by-date.js
@@ -1,11 +1,7 @@
 "use strict";
 
 async function getSalesOrdersByDate(transaction, range) {
-  throw new Error("NotImplementedError");
-  // STEP 1: Validation
-  // STEP 2: Create the data that the carrier's API expects
-  // STEP 3: Call the carrier's API
-  // STEP 4: Create the output data that ShipEngine expects
+  return undefined;
 }
 
 module.exports = getSalesOrdersByDate;
